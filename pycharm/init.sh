385b7f7b701bc4bf318eb0feed17af95033b8e5a
Mike Lambert <lambert8@illinois.edu>
2017-07-27 19:10:19 -0500

    Fix pycharm to use pycharm community edition 2017

--- pycharm/init.sh
+++ pycharm/init.sh
@@ -3,7 +3,7 @@ Xvfb +extension RANDR :0 -ac -screen 0 1280x720x16 > /dev/null &
 sleep 5
 
 x11vnc -forever -create -display :0 >/dev/null 2>&1 &
-/pycharm-community-2016.2/bin/pycharm.sh &
+/pycharm-community-2017.2/bin/pycharm.sh &
 sleep 10
 
 WID=`xwininfo -display :0 -root -children | grep "Complete Installation" | grep -Eo '0x[a-z0-9]+'`
